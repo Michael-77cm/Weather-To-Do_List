00ce0682a737fec40b4c0d1abe6976ee2e09c434
Michael-77cm <michaelbello175@gmail.com>
2026-03-17 08:53:53 +0000

    added '.herokuapp.com' to allowed hosts in settings.py

--- WeatherToDoList/config/settings.py
+++ WeatherToDoList/config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-+1+^(5o&s=@_d%s)_oe70$&v#*^c%3*b+@gv^qr+kx@cp&-4+(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env_bool('DJANGO_DEBUG', True)
 
-ALLOWED_HOSTS = [host for host in os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',') if host]
+ALLOWED_HOSTS = [host for host in os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',') if host] + ['.herokuapp.com']
 
 
 # Application definition
